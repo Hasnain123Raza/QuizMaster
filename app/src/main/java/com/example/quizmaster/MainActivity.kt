e40ab3386246f0c081b7975e4fe6958c00baa5e8
Hasnain Raza <hasnain.mohsan@gmail.com>
2020-12-25 20:43:26 -0500

    good job

--- app/src/main/java/com/example/quizmaster/MainActivity.kt
+++ app/src/main/java/com/example/quizmaster/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.quizmaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import com.example.quizmaster.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragment)
+
+        navController.addOnDestinationChangedListener {
+            controller, destination, arguments ->
+
+            title = when (destination.id) {
+                R.id.welcomeFragment -> "QuizMaster"
+                R.id.profileFragment -> "Profile"
+                R.id.viewQuizFragment -> "Quiz Title"
+                R.id.takeQuizFragment -> "Quiz Title"
+                R.id.quizResultFragment -> "Quiz Title"
+                R.id.aboutFragment -> "About"
+                else -> "QuizMaster"
+            }
+        }
     }
 }
